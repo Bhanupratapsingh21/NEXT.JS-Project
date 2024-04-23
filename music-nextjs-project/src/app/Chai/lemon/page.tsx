247eb0397c7e353abd6a730641ac493927c46ed1
Bhanupratapsingh21 <bhanupss137@gmail.com>
2024-04-23 16:45:30 +0530

    Navbar Accenternity UI Installation

--- music-nextjs-project/src/app/Chai/lemon/page.tsx
+++
@@ -1,7 +0,0 @@
-
-export default function Lemonpage() {
-    return (
-      <h1 className="2xl text-center">Lemon tea</h1>
-    );
-  }
-  
--- music-nextjs-project/src/app/Chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Chaipage(){
-    return (
-        <h2 className="text-3xl text-center mt-8">Chai Pages</h2>
-    )
-}
