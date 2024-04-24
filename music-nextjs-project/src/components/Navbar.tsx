57c874211ed51c04be18221a51c37a6c3292129c
Bhanupratapsingh21 <bhanupss137@gmail.com>
2024-04-24 15:15:40 +0530

    Hero-section Done

--- music-nextjs-project/src/components/Navbar.tsx
+++ music-nextjs-project/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
     return (
-        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+        <div className={cn("fixed  top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
             <Menu setActive={setActive}>
                 <Link href={"/"}>
                     <MenuItem setActive={setActive} active={active} item="Home">
@@ -35,7 +35,6 @@ function Navbar({ className }: { className?: string }) {
                 </MenuItem>
                 <Link href={"/contact"}>
                     <MenuItem setActive={setActive} active={active} item="Contact us">
-
                     </MenuItem>
                 </Link>
             </Menu>
